68af6e68b2620e62d1b91795778d6276c27716dc
Tarun Lywait <70788854+MysticalMist123@users.noreply.github.com>
2026-05-12 20:20:59 +0530

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int l=0,r=0;
+        int ans = 0;
+        unordered_map<char, int> char_occur;
+        while(r<s.size()){
+            char_occur[s[r]]++;
+            while(l<r && char_occur[s[r]]>1){
+                char_occur[s[l]]--;
+                l++;
+            }
+            cout<<l<<" "<<r<<"\n";
+            ans=max(r-l+1,ans);
+            r++;
+        }
+        return ans;
+    }
+};
